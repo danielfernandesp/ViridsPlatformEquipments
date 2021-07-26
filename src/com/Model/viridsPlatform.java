326552d67625b0ceb539cb63fcde4c204e3336e7
Daniel Fernandes <danielnrk3@gmail.com>
2021-07-26 10:01:23 -0300

    Initial commit

---
+++ src/com/Model/viridsPlatform.java
@@ -0,0 +1,30 @@
+package com.Model;
+
+public class viridsPlatform {
+    private int code;
+    private String name;
+
+    // Constructor for the class
+    public viridsPlatform(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Getters and setters methods
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
